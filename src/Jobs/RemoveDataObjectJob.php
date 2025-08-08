6ff16929c55650425450720e7276f5adf093008a
Chris Penny <chris.penny@gmail.com>
2025-08-08 15:17:25 +1200

    Forager support for Fluent in Silverstripe 6

---
+++ src/Jobs/RemoveDataObjectJob.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SilverStripe\ForagerFluent\Jobs;
+
+use InvalidArgumentException;
+use SilverStripe\Forager\Jobs\RemoveDataObjectJob as BaseRemoveDataObjectJob;
+use SilverStripe\ForagerFluent\Helpers\FluentHelper;
+use TractorCow\Fluent\State\FluentState;
+
+/**
+ * @property string|null $locale
+ */
+class RemoveDataObjectJob extends BaseRemoveDataObjectJob
+{
+
+    public function setup(): void
+    {
+        $this->extend('onBeforeFluentSetup');
+
+        if (!$this->getIndexSuffix()) {
+            throw new InvalidArgumentException('An index suffix must be specified');
+        }
+
+        $locale = FluentHelper::getLocaleForIndexSuffix($this->getIndexSuffix());
+
+        if (!$locale) {
+            throw new InvalidArgumentException(
+                sprintf('Unable to find Locale for index with suffix "%s"', $this->getIndexSuffix())
+            );
+        }
+
+        $this->setLocale($locale);
+
+        // Wrap our process in FluentState, so that the state is automatically reset at the end of the process
+        FluentState::singleton()->withState(function (FluentState $state): void {
+            // Set our Fluent state. This will mean that DataObjects are fetched for this specific Locale
+            $state->setLocale($this->getLocale());
+
+            parent::setup();
+        });
+
+        $this->extend('onAfterFluentSetup');
+    }
+
+    public function process(): void
+    {
+        $this->extend('onBeforeFluentProcess');
+
+        // Wrap our process in FluentState, so that the state is automatically reset at the end of the process
+        FluentState::singleton()->withState(function (FluentState $state): void {
+            // Set our Fluent state. This will mean that DataObjects are fetched for this specific Locale (and with the
+            // content from that Locale)
+            $state->setLocale($this->getLocale());
+
+            parent::process();
+        });
+
+        $this->extend('onAfterFluentProcess');
+    }
+
+    public function getLocale(): ?string
+    {
+        if (is_bool($this->locale)) {
+            return null;
+        }
+
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): static
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+}
---
+++ tests/Fake/FakeFluentVersioned.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SilverStripe\ForagerFluent\Tests\Fake;
+
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\Forager\Extensions\SearchServiceExtension as BaseSearchExtension;
+use SilverStripe\ForagerFluent\Extensions\SearchServiceExtension;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Versioned\Versioned;
+use TractorCow\Fluent\Extension\FluentVersionedExtension;
+
+/**
+ * @property string $Title
+ * @mixin FluentVersionedExtension
+ * @mixin SearchServiceExtension
+ * @mixin Versioned
+ */
+class FakeFluentVersioned extends DataObject implements TestOnly
+{
+
+    private static string $table_name = 'FakeFluentVersioned';
+
+    private static array $extensions = [
+        BaseSearchExtension::class,
+        Versioned::class,
+        FluentVersionedExtension::class,
+    ];
+
+    private static array $db = [
+        'Title' => 'Varchar',
+    ];
+
+    private static array $field_include = [
+        'ShowInSearch',
+    ];
+
+    public function canView(mixed $member = null): bool
+    {
+        return true;
+    }
+
+}
